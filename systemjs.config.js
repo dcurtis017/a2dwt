a5b727e6ac30f3c6efd7525f9586bb68e65ed3f1
= <mrjoefriday@gmail.com>
2017-02-13 21:38:47 -0500

    initial commit

---
+++ systemjs.config.js
@@ -0,0 +1,31 @@
+System.config({
+    transpiler: 'typescript',
+    typescriptOptions: {
+        emitDecoratorMetadata: true,
+        target: 'ES5',
+        module: 'commonjs'
+    },
+    map: {
+        '@angular': 'node_modules/@angular',
+        'rxjs': 'node_modules/rxjs'
+    },
+    paths: {
+        'node_modules/@angular/*': 'node_modules/@angular/*/bundles'
+    },
+    meta: {
+        '@angular/*': {'format': 'cjs'}
+    },
+    packages: {
+        'app': {main: 'main', defaultExtension: 'ts'},
+        'rxjs': {main: 'Rx'},
+        '@angular/core': {main: 'core.umd.min.js'},
+        '@angular/common': {main: 'common.umd.min.js'},
+        '@angular/compiler': {main: 'compiler.umd.min.js'},
+        '@angular/platform-browser': {main: 'platform-browser.umd.min.js'},
+        '@angular/platform-browser-dynamic': {main: 'platform-browser-dynamic.umd.min.js'}
+    }
+});
+//map allows you to create an alias for amodule name
+//the map parameter is applie dbefore the baseURL
+//a module can't span multiple files. A bundle is usually represented by a single file 
+//and contains multiple modules registered in it. 
